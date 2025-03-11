758d9e836d9a2e3af8fd4edb4702dbfc76c46bd7
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2025-03-11 22:13:17 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-let bMessage = 'Start typing or enter '*' to show all entries'
+//let bMessage = 'Start typing or enter '*' to show all entries'
 
-let nrMessage = 'No results, try a different query'
+//let nrMessage = 'No results, try a different query'
 
-let fsTerm = '*' 
+//let fsTerm = '*' 
   
